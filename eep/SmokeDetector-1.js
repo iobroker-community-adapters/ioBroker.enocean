758ebdec302aafcd50af11548c2bbae5ad3e7a3c
Sven Rehfuß <sven@rehfuss.org>
2017-12-26 13:12:35 +0100

    Bugfix for smokedetector.

--- eep/SmokeDetector-1.js
+++ eep/SmokeDetector-1.js
@@ -14,10 +14,10 @@ module.exports=function(telegram) {
     } else {
         // message
         var SA = data['SA'].value;
-        var R1 = data['R2'].value;
-        var EB = (data['EB'].value == 1);
+        if ((SA == 0) && (data['R1'] != undefined)) {
+            var R1 = data['R1'].value;
+            var EB = (data['EB'].value == 1);
 
-        if ((SA == 0) && (R1 != undefined)) {
             switch(R1) {
                 case 0:
                     if (EB) {
