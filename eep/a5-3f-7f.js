79beb3229d0c3a849c31c2b1fee60bf6a7fcd7d7
syryna <michael.novak@gmx.de>
2019-01-01 22:27:47 +0100

    added logic for a5-3f-7f in js and eepinclude.js

---
+++ eep/a5-3f-7f.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// required for autocompletion
+const RadioTelegram = require('../lib/esp3Packet').RadioTelegram;
+
+const T21_FLAG = 0b00100000;
+const NU_FLAG = 0b00010000;
+
+/** @typedef {"byte0"|"byte1"|"byte2"|"byte3"} DataPayload  */
+/** @type {DataPayload[]} */
+const DataPayload = ["byte0", "byte1", "byte2", "byte3"];
+
+/**
+ * @param {RadioTelegram} telegram 
+ */
+module.exports = function (telegram) {
+
+  let retValue = {};
+
+  const RORG = telegram.type;
+  retValue = {"RORG": RORG};
+//   const T21 = (telegram.status & T21_FLAG) === T21_FLAG;
+//   const NU = (telegram.status & NU_FLAG) === NU_FLAG;
+
+//   const dataField = telegram.userData[0];
+  
+//   if (T21 && !NU) {
+//     // this happens when the buttons are released
+//     const numButtons = (dataField & 0xE0) >> 5;
+//     const EB = ((dataField & 0x10) === 0x10);
+//     if (numButtons === 0 && !EB) retValue = {
+//       "AI": false, 
+//       "A0": false, 
+//       "BI": false, 
+//       "B0": false
+//     };
+
+//   } else if (T21 && NU) {
+//     // message
+//     const SA = dataField & 0x01;
+//     const R1 = (dataField & 0xE0) >> 5;
+//     const R2 = (dataField & 0x0E) >> 1;
+//     const EB = ((dataField & 0x10) === 0x10);
+
+//     retValue[RockerActions[R1]] = EB;
+//     if (SA) {
+//       retValue[RockerActions[R2]] = EB;      
+//     }
+//   }
+  return retValue;
+};
--- eep/eepInclude.js
+++ eep/eepInclude.js
@@ -4,6 +4,8 @@ var eepTable = {
     a5_07_01_native: require('./a5-07-01.js'),
     a5_12_01_native: require('./a5-12-01.js'),
     a5_14_0a_native: require('./a5-14-0a.js'),
+    a5_3f_7f_native: require('./a5-3f-7f.js'),
+    a5_3f_7f_FSB61NP: require('./a5-3f-7f.js'),
     d2_01_09_native: require('./d2-01-09.js'),
     d5_00_01_native: require('./d5-00-01.js'),
     f6_02_01_native: require('./f6-02-01.js'),
