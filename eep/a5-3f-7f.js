3c9a5403c1e8b9cab8ef97e122867b0dc9ef8e19
syryna <michael.novak@gmx.de>
2019-01-02 21:39:37 +0100

    improvements

--- eep/a5-3f-7f.js
+++ eep/a5-3f-7f.js
@@ -18,19 +18,19 @@ module.exports = function (telegram) {
   let retValue = {};
 
   const RORG = telegram.type.toString(16);
-  const DataByte0 = telegram.userData[0].toString(16);
-  const DataByte1 = telegram.userData[1].toString(16);
-  const DataByte2 = telegram.userData[2].toString(16);
-  const DataByte3 = telegram.userData[3].toString(16);
+  const DataByte0 = telegram.userData.data[0].toString(16);
+  const DataByte1 = telegram.userData.data[1].toString(16);
+  const DataByte2 = telegram.userData.data[2].toString(16);
+  const DataByte3 = telegram.userData.data[3].toString(16);
   const Status = telegram.status.toString(16)};
   
   // Distinguish between BS4 (a5) and RPS (f6)
   if (RORG == 0xA5){ 
     // BS4
-    retValue['data1'] = telegram.userData[0];
-    retValue['data2'] = telegram.userData[1];
-    retValue['data3'] = telegram.userData[2];
-    retValue['data4'] = telegram.userData[3];
+    retValue['data1'] = telegram.userData.data[0];
+    retValue['data2'] = telegram.userData.data[1];
+    retValue['data3'] = telegram.userData.data[2];
+    retValue['data4'] = telegram.userData.data[3];
 
   } else if (RORG == 0xF6) { 
     // RPS
